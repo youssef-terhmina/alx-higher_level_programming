b6f41083b25c96e29ff54da84cfd15b5a0afb9d4
youssef-terhmina <terhminayoussef@gmail.com>
2023-06-08 17:24:04 -0700

    up

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def add(x, y):
+    """My addition function
+    Args:
+        x: first integer
+        y: second integer
+    Returns:
+        The return value. x + y
+    """
+    return (x + y)
