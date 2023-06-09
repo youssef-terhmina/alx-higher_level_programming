572a1c2c8916098c2b6e9be5f07b45bb41446efb
youssef-terhmina <terhminayoussef@gmail.com>
2023-06-08 17:17:58 -0700

    done

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+def add(x, y):
+    """My addition function
+    Args:
+        x: first integer
+        y: second integer
+    Returns:
+        The return value. x + y
+    """
+    return (x + y)
+
+
+def sub(x, y):
+    """My subtraction function
+    Args:
+        x: first integer
+        y: second integer
+    Returns:
+        The return value. x - y
+    """
+    return (x - y)
+
+
+def mul(x, y):
+    """My multiplication function
+    Args:
+        x: first integer
+        y: second integer
+    Returns:
+        The return value. x * y
+    """
+    return (x * y)
+
+
+def div(x, y):
+    """My division function
+    Args:
+        x: first integer
+        y: second integer
+    Returns:
+        The return value. x / y
+    """
+    return int(x / y)
