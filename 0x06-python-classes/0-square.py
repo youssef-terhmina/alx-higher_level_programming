f2c09ee8292ba5a4499c0879a146354edb2fb640
youssef-terhmina <terhminayoussef@gmail.com>
2023-06-27 15:53:42 -0700

    0/1/2

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""Define class Square"""
+
+class Square:
+
+    """Represent square"""
+
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Define class Square"""
+
+class Square:
+
+    """Represent square"""
+
+    def __init__(self, size):
+
+        """Initialize new Square
+        Args:
+            size (int): The size of the new square"""
+
+        self.__size = size
